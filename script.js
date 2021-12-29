db12974738dd533ff89be17abf6e76040c6692d3
PaulMolochko <paulmolochko@outlook.com>
2021-12-29 17:32:25 +0200

    added interractivity to other doors. removed one door, as the task specified 3 doors only

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const doorImage1 = document.getElementById("door1");
+const doorImage2 = document.getElementById("door2");
+const doorImage3 = document.getElementById("door3");
 
 const botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
 
 doorImage1.onclick = () =>{
   doorImage1.src = botDoorPath;
+}
+doorImage2.onclick = () =>{
+    doorImage2.src = botDoorPath;
+  }
+doorImage3.onclick = () =>{
+  doorImage3.src = botDoorPath;
 }
