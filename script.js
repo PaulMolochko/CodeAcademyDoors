49723ef0d950f792c42c44fa226d91a1a1231438
PaulMolochko <paulmolochko@outlook.com>
2021-12-29 17:47:41 +0200

    changed the door images

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ const doorImage3 = document.getElementById("door3");
 
 const botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
 
+const beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg"
+
+const spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg"
+
+
 doorImage1.onclick = () =>{
   doorImage1.src = botDoorPath;
 }
 doorImage2.onclick = () =>{
-    doorImage2.src = botDoorPath;
+    doorImage2.src = beachDoorPath;
   }
 doorImage3.onclick = () =>{
-  doorImage3.src = botDoorPath;
+  doorImage3.src = spaceDoorPath;
 }
