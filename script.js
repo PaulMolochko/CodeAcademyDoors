478b65cf1e24d5e1cafd547cdecf66a102f0474a
PaulMolochko <paulmolochko@outlook.com>
2021-12-30 10:25:13 +0200

    added randomiser to change the door images

--- script.js
+++ script.js
@@ -10,11 +10,38 @@ const spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images
 
 
 doorImage1.onclick = () =>{
-  doorImage1.src = botDoorPath;
+  doorImage1.src = openDoor1;
 }
 doorImage2.onclick = () =>{
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
   }
 doorImage3.onclick = () =>{
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
+}
+
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+    const numClosedDoors = 3;
+    const choreDoor = Math.floor(Math.random()*numClosedDoors);
+    console.log(choreDoor)
+    switch (choreDoor) {
+        case 0:
+            openDoor1 = botDoorPath;
+            openDoor2 = beachDoorPath; 
+            openDoor3 = spaceDoorPath;
+            break;
+        case 1:
+            openDoor1 = beachDoorPath; 
+            openDoor2 = botDoorPath;
+            openDoor3 = spaceDoorPath; 
+            break;
+        case 2:
+            openDoor1 = beachDoorPath; 
+            openDoor2 = spaceDoorPath; 
+            openDoor3 = botDoorPath;
+            break;}
+    console.table([openDoor1,openDoor2,openDoor3]);
 }
